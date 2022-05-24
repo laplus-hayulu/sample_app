05f429c5c605d5375786a0d94826d20d3e0083aa
manab oz <staff@oz-edu.com>
2022-05-24 17:10:24 +0900

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
+  root 'static_pages#home'
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
 end
